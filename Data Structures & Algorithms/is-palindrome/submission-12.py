084db2c30b159d125641795bc976006654122910
Sachin Jadhav <143353582+robosac333@users.noreply.github.com>
2026-05-28 16:11:34 +0530

    Add: is-palindrome - submission-12

---
+++ Data Structures & Algorithms/is-palindrome/submission-12.py
@@ -0,0 +1,6 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        stri = "".join(l for l in s.split(" "))
+        stri = "".join(l.lower() if l.isalnum() else "" for l in list(stri))
+        return stri == stri[::-1]
